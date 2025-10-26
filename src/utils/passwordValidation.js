985bcfee231e54b50e7dae29d8e7d55340b9616a
pree-sips-her-tea <priyamm.007.parashar@gmail.com>
2025-10-26 18:14:50 +0530

    Add files via upload

---
+++ src/utils/passwordValidation.js
@@ -0,0 +1,46 @@
+export default function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+    return null;
+}const passwordValidation = {
+    minLength: 8,
+    requireUppercase: true,
+    requireLowercase: true,
+    requireNumber: true,
+    requireSpecialCharacter: true,
+};
+
+export const validatePassword = (password, setPasswordMessage) => {
+    const hasUppercase = /[A-Z]/.test(password);
+    const hasLowercase = /[a-z]/.test(password);
+    const hasNumber = /[0-9]/.test(password);
+    const hasSpecialCharacter = /[!@#$%^&*()_+-=\[\]{};':"|\\,.<>/ ?]/.test(password);
+
+    let valid = true;
+    let messages = [];
+
+    if (password.length < passwordValidation.minLength) {
+        valid = false;
+        messages.push(`Password must be at least ${passwordValidation.minLength} characters long`);
+    }
+    if (!hasUppercase && passwordValidation.requireUppercase) {
+        valid = false;
+        messages.push("Password must include at least one uppercase letter (A-Z)");
+    }
+    if (!hasLowercase && passwordValidation.requireLowercase) {
+        valid = false;
+        messages.push("Password must include at least one lowercase letter (a-z)");
+    }
+    if (!hasNumber && passwordValidation.requireNumber) {
+        valid = false;
+        messages.push("Password must include at least one number (0-9)");
+    }
+    if (!hasSpecialCharacter && passwordValidation.requireSpecialCharacter) {
+        valid = false;
+        messages.push("Password must include at least one special character (!@#$%^&*)");
+    }
+
+    setPasswordMessage(valid ? "" : messages.join("\n"));
+    return valid;
+}
